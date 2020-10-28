00fa58a9286ef9936ae8f8979321cdc77d980595
ODND <70161933+ODND-Open-Source@users.noreply.github.com>
2020-10-28 10:59:49 +0100

    Update menu.c

--- source/menu.c
+++ source/menu.c
@@ -23,4 +23,39 @@ int showMainMenu()
 	OSScreenPutFontEx(SCREEN_DRC, 0, 0, "StocksU Dashboard");
 	OSScreenPutFontEx(SCREEN_DRC, 0, 1, "--------------------------------------");
 	OSScreenPutFontEx(SCREEN_DRC, 0, 2, "Please choose a option:");
+	
+	// Drawing to the screen depending on where the "cursor" is
+	if (cursPos == 1) {
+		OSScreenPutFontEx(SCREEN_TV, 0, 3, "<Company List>");
+		OSScreenPutFontEx(SCREEN_TV, 0, 4, "Wallet");
+		OSScreenPutFontEx(SCREEN_TV, 0, 5, "Buy Stock");
+		OSScreenPutFontEx(SCREEN_TV, 0, 6, "Sell Stock");
+		
+		OSScreenPutFontEx(SCREEN_DRC, 0, 3, "<Company List>");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 4, "Wallet");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 5, "Buy Stock");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 5, "Sell Stock");
+	}
+	if (cursPos == 2) {
+		OSScreenPutFontEx(SCREEN_TV, 0, 3, "Pound Sterling");
+		OSScreenPutFontEx(SCREEN_TV, 0, 4, "<US Dollar>");
+		OSScreenPutFontEx(SCREEN_TV, 0, 5, "EU Euro");
+		
+		OSScreenPutFontEx(SCREEN_DRC, 0, 3, "Pound Sterling");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 4, "<US Dollar>");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 5, "EU Euro");
+	}
+	if (cursPos == 3) {
+		OSScreenPutFontEx(SCREEN_TV, 0, 3, "Pound Sterling");
+		OSScreenPutFontEx(SCREEN_TV, 0, 4, "US Dollar");
+		OSScreenPutFontEx(SCREEN_TV, 0, 5, "<EU Euro>");
+		
+		OSScreenPutFontEx(SCREEN_DRC, 0, 3, "Pound Sterling");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 4, "US Dollar");
+		OSScreenPutFontEx(SCREEN_DRC, 0, 5, "<EU Euro>");
+	}
+	OSScreenPutFontEx(SCREEN_DRC, 0, 6, "--------------------------------------");
+	OSScreenPutFontEx(SCREEN_DRC, 0, 6, "Press A to select.");
+	OSScreenPutFontEx(SCREEN_TV, 0, 6, "--------------------------------------");
+	OSScreenPutFontEx(SCREEN_TV, 0, 6, "Press A to select.");
 }
